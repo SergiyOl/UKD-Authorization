89578ea70ea55306f6f8d68c54ce2b8db2bdb684
Anna05a <117213428+Anna05a@users.noreply.github.com>
2023-05-03 15:05:32 +0300

    fixet bugs

--- Task.js
+++ Task.js
@@ -32,51 +32,60 @@ function mailOptions(mailTo, mailSubject, mailText) {
     return mailOptions
 }
 
-function userRegistration(filePath) {
-    console.log("Почато процес реєстрації")
-    let data = fs.readFileSync(filePath, 'utf8')
-    data = data.split('\r\n')
-    while (true) {
-        let email = prompt("Введіть адресу вашої пошти (Напишіть < exit > щоб припинити реєстрацію): ")
-        if (email == "exit"){
-            console.log("Припиняємо процес реєстрації")
-            return
-        }
-        for (let i = 0; i < data.length; i++) {
-            if (email == data[i].split(',')[0]) {
-                console.log(`Користувач ${email} вже зареэстрований`)
+const userRegistration = (filePath) => {
+    return new Promise ((resolve) => {
+        console.log("Почато процес реєстрації")
+        let data = fs.readFileSync(filePath, 'utf8')
+        data = data.split('\r\n')
+        while (true) {
+            let email = prompt("Введіть адресу вашої пошти (Напишіть < exit > щоб припинити реєстрацію): ")
+            if (email == "exit"){
+                console.log("Припиняємо процес реєстрації")
+                resolve();
                 return
-            } else {
-                let password = prompt("Введіть пароль: ")
-                const salt = crypto.randomBytes(16).toString('hex')
-                const token = otp.authenticator.generate(salt)
-                console.log("Відправляємо лист підтвердження")
-                transporter.sendMail(mailOptions(email, "Підтвердження реєстрації", `Код підтвердження ${token}`), function (err) {
-                    if (err) {
-                        console.log("Error " + err);
-                        return
-                    } else {
-                        console.log("Лист підтвердження успішно відправлено")
-                        for (let i = 2; i >= 0; i--) {
-                            if (prompt("Введіть код підтвердження: ") == token) {
-                                let fileContent = `${email},${password}`
-                                fs.appendFile(filePath, fileContent + '\r\n', () => {
-                                    console.log(`Користувача зареєстровано`)
-                                })
-                                break
-                            } else {
-                                console.log(`Неправильний код підтвердження. Залишилось спроб ${i}`)
-                                if (i == 0) {
-                                    console.log(`Не вдалося зареєструвати користувача`)
-                                    break
+            }
+            for (let i = 0; i < data.length; i++) {
+                if (email == data[i].split(',')[0]) {
+                    console.log(`Користувач ${email} вже зареэстрований`)
+                    resolve();
+                    return
+                } else {
+                    let password = prompt("Введіть пароль: ")
+                    const salt = crypto.randomBytes(16).toString('hex')
+                    const token = otp.authenticator.generate(salt)
+                    console.log("Відправляємо лист підтвердження")
+                    transporter.sendMail(mailOptions(email, "Підтвердження реєстрації", `Код підтвердження ${token}`), function (err) {
+                        if (err) {
+                            console.log("Error " + err);
+                            resolve();
+                            return
+                        } else {
+                            console.log("Лист підтвердження успішно відправлено")
+                            for (let j = 2; j >= 0; j--) {
+                                if (prompt("Введіть код підтвердження: ") == token) {
+                                    let fileContent = `${email},${password}`
+                                    fs.appendFile(filePath, fileContent + '\r\n', () => {
+                                        console.log(`Користувача зареєстровано`)
+                                        resolve();
+                                        return;
+                                    })
+                                    return;
+                                } else {
+                                    console.log(`Неправильний код підтвердження. Залишилось спроб ${j}`)
+                                    if (j == 0) {
+                                        console.log(`Не вдалося зареєструвати користувача`)
+                                        resolve();
+                                        return;
+                                    }
                                 }
                             }
                         }
-                    }
-                });
+                    });
+                    return
+                }
             }
         }
-    }
+    })
 }
 
 const userLogIn = (filePath) => {
@@ -121,54 +130,60 @@ const userLogIn = (filePath) => {
     })
 }
 
-function userPaswordReset(filePath) {
-    console.log("Почато процес скидання паролю")
-    let data = fs.readFileSync(filePath, 'utf8')
-    data = data.split('\r\n')
-    while (true) {
-        let email = prompt("Введіть адресу вашої пошти (Напишіть < exit > щоб припинити скидання паролю): ")
-        if (email == "exit"){
-            console.log("Припиняємо процес скидання паролю")
-            return
-        }
-        for (let i = 0; i < data.length; i++) {
-            if (email == data[i].split(',')[0]) {
-                const salt = crypto.randomBytes(16).toString('hex')
-                const token = otp.authenticator.generate(salt)
-                console.log("Відправляємо лист підтвердження")
-                transporter.sendMail(mailOptions(email, "Скидання паролю", `Код підтвердження ${token}`), function (err) {
-                    if (err) {
-                        console.log("Error " + err);
-                        return
-                    } else {
-                        console.log("Лист підтвердження успішно відправлено")
-                        for (let j = 2; j >= 0; j--) {
-                            if (prompt("Введіть код підтвердження: ") == token) {
-                                let newPassword = prompt("Введіть новий пароль: ")
-                                data[i] = `${email},${newPassword}`;
-                                fs.writeFileSync(filePath, data.join('\r\n'))
-                                console.log(`Пароль успішно змінено на ${newPassword}`)
-                                return
-                            } else {
-                                console.log(`Неправильний код підтвердження. Залишилось спроб ${j}`)
-                                if (j == 0) {
-                                    console.log(`Не вдалося підтвердити користувача`)
+const userPaswordReset = (filePath) => {
+    return new Promise ((resolve) => {
+        console.log("Почато процес скидання паролю")
+        let data = fs.readFileSync(filePath, 'utf8')
+        data = data.split('\r\n')
+        while (true) {
+            let email = prompt("Введіть адресу вашої пошти (Напишіть < exit > щоб припинити скидання паролю): ")
+            if (email == "exit"){
+                console.log("Припиняємо процес скидання паролю")
+                resolve();
+                return
+            }
+            for (let i = 0; i < data.length; i++) {
+                if (email == data[i].split(',')[0]) {
+                    const salt = crypto.randomBytes(16).toString('hex')
+                    const token = otp.authenticator.generate(salt)
+                    console.log("Відправляємо лист підтвердження")
+                    transporter.sendMail(mailOptions(email, "Скидання паролю", `Код підтвердження ${token}`), function (err) {
+                        if (err) {
+                            console.log("Error " + err);
+                            resolve();
+                            return
+                        } else {
+                            console.log("Лист підтвердження успішно відправлено")
+                            for (let j = 2; j >= 0; j--) {
+                                if (prompt("Введіть код підтвердження: ") == token) {
+                                    let newPassword = prompt("Введіть новий пароль: ")
+                                    data[i] = `${email},${newPassword}`;
+                                    fs.writeFileSync(filePath, data.join('\r\n'))
+                                    console.log(`Пароль успішно змінено на ${newPassword}`)
+                                    resolve();
                                     return
+                                } else { 
+                                    console.log(`Неправильний код підтвердження. Залишилось спроб ${j}`)
+                                    if (j == 0) {
+                                        console.log(`Не вдалося підтвердити користувача`)
+                                        resolve();
+                                        return
+                                    }
                                 }
                             }
                         }
-                    }
-                });
-                return;
+                    });
+                    return;
+                }
             }
+           console.log(`Користувача ${email} не знайдено`)
         }
-       console.log(`Користувача ${email} не знайдено`)
-    }
-
+    })
 }
 
 // Запуск
-userRegistration(filePath)
-userLogIn(filePath).then(() => {
-    userPaswordReset(filePath);
+userRegistration(filePath).then(() => {
+    userLogIn(filePath).then(() => {
+        userPaswordReset(filePath);
+    })
 })
